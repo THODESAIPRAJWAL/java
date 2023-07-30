0a57e2261ec87f669661fd73a94ae19094ec8b10
Thode Sai prajwal <110177534+THODESAIPRAJWAL@users.noreply.github.com>
2023-07-30 08:59:35 +0530

    math

--- math/RectangleArea.java
+++ math/RectangleArea.java
@@ -3,7 +3,7 @@ package math;
 public class RectangleArea {
     
 }
-lass Solution {
+class Solution {
     public int computeArea(int ax1, int ay1, int ax2, int ay2, int bx1, int by1, int bx2, int by2) {
        // Calculate the areas of the two rectangles separately
        int areaA = Math.abs(ax1 - ax2) * Math.abs(ay1 - ay2);

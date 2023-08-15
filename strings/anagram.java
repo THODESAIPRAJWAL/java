a7bb216af82599b6592a1a8e14481234cef0029f
Thode Sai prajwal <110177534+THODESAIPRAJWAL@users.noreply.github.com>
2023-08-15 20:14:17 +0530

    string

--- strings/anagram.java
+++ strings/anagram.java
@@ -4,14 +4,46 @@ public class anagram {
         Scanner sc=new Scanner(System.in);
         String s=sc.nextLine();
         String t=sc.nextLine();
-        if(s.length()==t.length()){
-        StringBuilder ana=new StringBuilder(s);
-        int i=0;
-        while(ana.length()!=null){
-            for(int j=0;j<t.length();j++){
-            t.charAt(i)
+        
+        }
+    }
+}
+import java.util.HashMap;
+import java.util.Map;
+
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        
+        // Create a frequency map for characters in both strings
+        Map<Character, Integer> freqMap = new HashMap<>();
+        
+        // Count the frequency of characters in string s
+        for (char c : s.toCharArray()) {
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+        }
+        
+        // Decrement the frequency of characters in string t
+        for (char c : t.toCharArray()) {
+            if (!freqMap.containsKey(c)) {
+                return false; // Character not found in s
+            }
+            
+            freqMap.put(c, freqMap.get(c) - 1);
+            
+            if (freqMap.get(c) == 0) {
+                freqMap.remove(c);
             }
         }
-        }
+        
+        return freqMap.isEmpty(); // If all characters have been used up
+    }
+
+    public static void main(String[] args) {
+        ValidAnagram validAnagram = new ValidAnagram();
+        System.out.println(validAnagram.isAnagram("anagram", "nagaram")); // Output: true
+        System.out.println(validAnagram.isAnagram("rat", "car")); // Output: false
     }
 }

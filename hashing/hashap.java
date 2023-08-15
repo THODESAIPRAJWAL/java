470ea13b72c32f3b05489ce7bcb4f6bae966908b
Thode Sai prajwal <110177534+THODESAIPRAJWAL@users.noreply.github.com>
2023-08-15 19:19:31 +0530

    hash

---
+++ hashing/hashap.java
@@ -0,0 +1,20 @@
+// Java program to traversal a
+// Java.util.HashMap
+  
+import java.util.HashMap;
+import java.util.Map;
+  
+public class TraversalTheHashMap {
+    public static void main(String[] args)
+    {
+
+        HashMap<Integer,String> n=new HashMap<>();
+        n.put(1,"sai");
+        n.put(2,"saiprajwal");
+        n.put(3,"saireddy");
+        for(Map.Entry<Integer,String> e :n.entrySet()) {
+            System.out.println(e.getKey());
+            System.out.println(e.getValue());
+
+    }
+}
---
+++ hashing/int.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class solution{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int[] num=new int[10];
+        for(int i=0;i<10;i++){
+            num[i]=sc.nextInt();
+        }
+        
+    }
+}

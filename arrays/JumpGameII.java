551448143cf61ddd69921ce2866669b6d2a3adc1
Thode Sai prajwal <110177534+THODESAIPRAJWAL@users.noreply.github.com>
2023-08-30 21:29:58 +0530

    a

---
+++ arrays/JumpGameII.java
@@ -0,0 +1,10 @@
+public class JumpGameII {
+    public static int jump(int[] nums) {
+        
+    }
+    public static void main(String[] args) {
+        int[] nums ={2,3,1,1,4};
+       System.out.print(jump(nums));
+
+    }
+}

c27aa0ecc807b0eeba997e1399673768c19b4d05
Thode Sai prajwal <110177534+THODESAIPRAJWAL@users.noreply.github.com>
2024-07-02 08:22:04 +0530

    code

--- arrays/TwoSumII.java
+++ arrays/TwoSumII.java
@@ -1,26 +1,61 @@
+// public class TwoSumII{
+//     public static int[] twoSum(int[] numbers, int target) {
+//        int left=0;
+//        int right=numbers.length-1;
+//        while(left<right){
+//         int sum=numbers[left]+numbers[right];
+//         if(sum==target){
+//             return new int[]{left+1,right+1};
+//         }
+//         else if(sum<target){
+//             left++;
+//         }
+//         else{
+//             right--;
+//         }
+//        }
+//        return new int[0];
+//     }
+//     public static void main(String[] args) {
+//         int[] nums ={2,7,11,15};
+//         int[] a=twoSum(nums,9);
+//         for(int aa:a){
+//             System.out.println(aa);
+//         }
+//     }
+// }
 public class TwoSumII {
     public static int[] twoSum(int[] numbers, int target) {
-       int left=0;
-       int right=numbers.length-1;
-       while(left<right){
-        int sum=numbers[left]+numbers[right];
-        if(sum==target){
-            return new int[]{left+1,right+1};
+        // Two pointers approach
+        int left = 0;
+        int right = numbers.length - 1;
+        
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            
+            if (sum == target) {
+                // Indices are expected to be 1-based
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
         }
-        else if(sum<target){
-            left++;
-        }
-        else{
-            right--;
-        }
-       }
-       return new int[0];
+        
+        // Return an empty array if no solution is found
+        return new int[0];
     }
+    
     public static void main(String[] args) {
-        int[] nums ={2,7,11,15};
-        int[] a=twoSum(nums,9);
-        for(int aa:a){
-            System.out.println(aa);
+        int[] nums = {2, 7, 11, 15};
+        int[] result = twoSum(nums, 9);
+        
+        if (result.length > 0) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No solution found.");
         }
     }
 }
+
---
+++ arrays/practice.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class practice {
+    public static void main(Strings[] args){
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        System.out.println(a);
+    }
+}
